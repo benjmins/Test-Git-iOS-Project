d6e2b070d413486c6bbb5dcf0eb6c63ff0d86bda
benjmins <benjmin.ho@gmail.com>
2014-02-21 14:03:10 -0500

    added a test class

---
+++ BHTest.h
@@ -0,0 +1,13 @@
+//
+//  BHTest.h
+//  Git Test
+//
+//  Created by Benjamin on 2/21/2014.
+//  Copyright (c) 2014 Benjamin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BHTest : NSObject
+
+@end
---
+++ BHTest.m
@@ -0,0 +1,13 @@
+//
+//  BHTest.m
+//  Git Test
+//
+//  Created by Benjamin on 2/21/2014.
+//  Copyright (c) 2014 Benjamin. All rights reserved.
+//
+
+#import "BHTest.h"
+
+@implementation BHTest
+
+@end
