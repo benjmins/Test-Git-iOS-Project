3acdd8508aa3a1e3a5808971030f900a520d1c39
benjmins <benjmin.ho@gmail.com>
2014-02-21 13:53:02 -0500

    Initial Commit

---
+++ Git Test/BHViewController.h
@@ -0,0 +1,13 @@
+//
+//  BHViewController.h
+//  Git Test
+//
+//  Created by Benjamin on 2/21/2014.
+//  Copyright (c) 2014 Benjamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BHViewController : UIViewController
+
+@end
---
+++ Git Test/BHViewController.m
@@ -0,0 +1,29 @@
+//
+//  BHViewController.m
+//  Git Test
+//
+//  Created by Benjamin on 2/21/2014.
+//  Copyright (c) 2014 Benjamin. All rights reserved.
+//
+
+#import "BHViewController.h"
+
+@interface BHViewController ()
+
+@end
+
+@implementation BHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
